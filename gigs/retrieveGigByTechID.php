96300ad83647b51508a5f1a321770c3eac07a1ac
King Richard Guevara <42199746+kingrgdev@users.noreply.github.com>
2025-03-25 06:05:40 +0800

    gig per technician -> per day

--- gigs/retrieveGigByTechID.php
+++ gigs/retrieveGigByTechID.php
@@ -75,7 +75,7 @@ $sql = "
     FROM gigs g
     INNER JOIN clients c ON g.client_id = c.client_id
     INNER JOIN users u ON g.assigned_tech_id = u.id
-    WHERE g.assigned_tech_id = $technician
+    WHERE g.assigned_tech_id = $technician AND DATE(g.created_at) = CURDATE()
     ORDER BY g.start_datetime DESC
 ";
 
