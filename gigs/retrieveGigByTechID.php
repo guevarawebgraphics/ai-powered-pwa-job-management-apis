b1b2ec3ed412b0af2db81dd09baa8f1653b6027f
King Richard Guevara <42199746+kingrgdev@users.noreply.github.com>
2025-04-01 04:52:01 +0800

    fixes

--- gigs/retrieveGigByTechID.php
+++ gigs/retrieveGigByTechID.php
@@ -28,17 +28,85 @@ $dbConn->set_charset("utf8mb4");
 
 $technician = intval($input_data['techID']);
 
-// Use provided date or default to current date if not provided
 $date_filter = isset($input_data['date']) && !empty($input_data['date'])
     ? $input_data['date']
     : date('Y-m-d');
 
-// Optionally use provided time filter (should be in 24-hour format, e.g., "14:30")
 $time_filter = isset($input_data['time']) && !empty($input_data['time'])
     ? $input_data['time']
     : null;
 
-// Start building the SQL query
+    
+// Filter by Date & Time
+// $sql = "
+//     SELECT 
+//         g.gig_id,
+//         g.gig_cryptic,
+//         g.machine_brand,
+//         g.appliance_type,
+//         g.model_number,
+//         g.customer_input,
+//         g.serial_number,
+//         g.initial_issue,
+//         g.top_recommended_repairs,
+//         g.gig_price,
+//         g.gig_price_detail,
+//         g.gig_discount,
+//         g.trainee_included,
+//         g.resolution,
+//         g.start_datetime,
+//         g.repair_notes,
+//         g.qb_invoice_num,
+//         g.child_of_gig,
+//         g.invoice_paid,
+//         g.gig_complete,
+//         g.parts_used,
+//         g.time_started,
+//         g.time_ended,
+//         g.extra_field1 AS gig_extra_field1,
+//         g.extra_field2 AS gig_extra_field2,
+//         g.created_at,
+//         g.updated_at,
+//         g.youtube_link,
+
+//         c.client_id,
+//         c.client_name,
+//         c.client_last_name,
+//         c.email AS client_email,
+//         c.phone_number AS client_phone_number,
+//         c.street_address,
+//         c.city,
+//         c.state,
+
+//         u.id AS tech_id,
+//         u.name AS tech_name,
+//         u.email AS tech_email
+//     FROM gigs g
+//     INNER JOIN clients c ON g.client_id = c.client_id
+//     INNER JOIN users u ON g.assigned_tech_id = u.id
+//     WHERE g.assigned_tech_id = ? 
+// ";
+
+// if ($time_filter) {
+//     $sql .= "AND DATE(g.start_datetime) = ? AND TIME(g.start_datetime) >= ? ";
+// } else {
+//     $sql .= "AND DATE(g.start_datetime) = ? ";
+// }
+
+
+// $sql .= "AND g.gig_complete != 3
+//     ORDER BY g.start_datetime DESC";
+
+// $stmt = $dbConn->prepare($sql);
+
+// if ($time_filter) {
+//     $stmt->bind_param("iss", $technician, $date_filter, $time_filter);
+// } else {
+//     $stmt->bind_param("is", $technician, $date_filter);
+// }
+
+$current_datetime = $input_data['current_datetime']; // UTC Time passed from Frontend Axios Vue
+
 $sql = "
     SELECT 
         g.gig_id,
@@ -86,26 +154,20 @@ $sql = "
     INNER JOIN clients c ON g.client_id = c.client_id
     INNER JOIN users u ON g.assigned_tech_id = u.id
     WHERE g.assigned_tech_id = ? 
-";
-
-// If time filter is provided, you might want to show all gigs from today that have a start_datetime
-// time greater than or equal to the provided time. Otherwise, filter only by the date.
-if ($time_filter) {
-    $sql .= "AND DATE(g.start_datetime) = ? AND TIME(g.start_datetime) >= ? ";
-} else {
-    $sql .= "AND DATE(g.start_datetime) = ? ";
-}
-
-$sql .= "AND g.gig_complete != 3
+      AND DATE(g.start_datetime) = ?
+      AND ? < DATE_ADD(g.start_datetime, INTERVAL 2 HOUR)
+      AND g.gig_complete != 3
     ORDER BY g.start_datetime DESC";
 
-$stmt = $dbConn->prepare($sql);
-
-if ($time_filter) {
-    $stmt->bind_param("iss", $technician, $date_filter, $time_filter);
-} else {
-    $stmt->bind_param("is", $technician, $date_filter);
+    // If total_records is provided and valid, append the LIMIT clause directly
+if (isset($input_data['total_records']) && is_numeric($input_data['total_records'])) {
+    $total_records = intval($input_data['total_records']);
+    $sql .= " LIMIT " . $total_records;
 }
+
+
+$stmt = $dbConn->prepare($sql);
+$stmt->bind_param("iss", $technician, $date_filter, $current_datetime);
 
 $stmt->execute();
 $result = $stmt->get_result();
